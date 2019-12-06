65f246335e0e71385971a61e3c1699fb50090ff5
Tommaso Allegri <allegritommaso@hotmail.it>
2019-12-06 09:44:38 +0000

    Added drawing
    Bugfixing and add drawing

---
+++ cpu/board.h
@@ -0,0 +1,17 @@
+#include <stdint.h>
+
+#define BOARD_ROWS 6
+#define BOARD_COLS 7
+
+typedef struct board{
+    int matrix[BOARD_ROWS][BOARD_COLS];
+} board_t;
+
+board_t *board_init();
+
+int get_disc(board_t *board, int y, int x);
+
+int place_disc(board_t *board, int x, int value);
+int get_elements_in_column(board_t *board, int x);
+
+int check_four(board_t *board, int start_x, int start_y);
